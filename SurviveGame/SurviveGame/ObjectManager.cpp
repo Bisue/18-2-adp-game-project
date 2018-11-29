1af633e469da43c7d5a957ce800b2ca5c18acb14
Bisue <bdu00chch@naver.com>
2018-11-29 21:18:19 +0900

    SavePointUpdate

---
+++ SurviveGame/SurviveGame/ObjectManager.cpp
@@ -0,0 +1,91 @@
+#include "ObjectManager.h"
+#include "Player.h"
+#include "Bullet.h"
+#include "Monster.h"
+#include "Item.h"
+#include "CollisionManager.h"
+#include "SoundManager.h"
+#include "ScoreManager.h"
+#include "Constants.h"
+
+namespace jm
+{
+	ObjectManager::ObjectManager(const vec2& playerStartPoint, const float& playerSpeed)
+	{
+		player = std::make_shared<Player>(playerStartPoint, playerSpeed);
+	}
+	void ObjectManager::addBullet(Bullet& bullet)
+	{
+		bullets.push_back(std::make_shared<Bullet>(bullet));
+	}
+	void ObjectManager::addItem(Item& item)
+	{
+		items.push_back(std::make_shared<Item>(item));
+	}
+	template <class MobType>
+	void ObjectManager::addMonster(MobType& monster)
+	{
+		monsters.push_back(std::shared_ptr<MobType>(monster));
+	}
+
+	void ObjectManager::update()
+	{
+		updateBullets();
+		updateMonsters();
+		updateItems();
+	}
+	void ObjectManager::updateBullets()
+	{
+		for (auto iter = bullets.begin(); iter < bullets.end();)
+		{
+			auto bullet = *iter;
+			if (!((bullet->getPos().x >= -SCREENBORDER && bullet->getPos().x <= SCREENBORDER) && (bullet->getPos().y >= -SCREENBORDER && bullet->getPos().y <= SCREENBORDER)))
+			{
+				iter = bullets.erase(iter);
+			}
+			else
+			{
+				iter++;
+			}
+		}
+	}
+	void ObjectManager::updateMonsters()
+	{
+		for (auto iterM = monsters.begin(); iterM < monsters.end(); iterM++)
+		{
+			auto monster = *iterM;
+			for (auto iterB = bullets.begin(); iterB < bullets.end(); iterB++)
+			{
+				auto bullet = *iterB;
+				if (CM::getInstance()->checkCircleCollision(monster.get(), bullet.get()))
+				{
+					SM::getInstance()->stopAndPlaySound("zombieHit");
+
+					float damage = bullet->getDamage();
+					iterB = bullets.erase(iterB) - 1;
+					monster->hit(damage);
+					if (monster->checkDie())
+					{
+						SM::getInstance()->stopAndPlaySound("zombieDie");
+
+						iterM = monsters.erase(iterM) - 1;
+						ScoreManager::getInstance()->addKill(1);
+						break;
+					}
+				}
+			}
+		}
+	}
+	void ObjectManager::updateItems()
+	{
+		for (auto iter = items.begin(); iter < items.end(); iter++)
+		{
+			auto item = *iter;
+			if (CM::getInstance()->checkCircleCollision(player.get(), item.get()))
+			{
+				iter = items.erase(iter) - 1;
+				player->increasePower();
+			}
+		}
+	}
+}
--- SurviveGame/SurviveGame/ObjectManager.h
+++ SurviveGame/SurviveGame/ObjectManager.h
@@ -1,18 +1,15 @@
 #pragma once
-#include "Player.h"
-#include "Bullet.h"
-#include "Monster.h"
-#include "Zombie.h"
-#include "Item.h"
-#include "CollisionManager.h"
-#include "SoundManager.h"
-#include "ScoreManager.h"
-#include "Constants.h"
+#include "Game2D.h"
 #include <vector>
 #include <memory>
 
 namespace jm
 {
+	class Player;
+	class Bullet;
+	class Monster;
+	class Item;
+
 	class ObjectManager
 	{
 	private:
@@ -23,85 +20,17 @@ namespace jm
 
 	public:
 		//constructor (for init player)
-		ObjectManager(const vec2& playerStartPoint, const float& playerSpeed)
-		{
-			player = std::make_shared<Player>(playerStartPoint, playerSpeed);
-		}
+		ObjectManager(const vec2& playerStartPoint, const float& playerSpeed);
 		
 		//add
-		void addBullet(Bullet& bullet)
-		{
-			bullets.push_back(std::make_shared<Bullet>(bullet));
-		}
-		void addItem(Item& item)
-		{
-			items.push_back(std::make_shared<Item>(item));
-		}
+		void addBullet(Bullet& bullet);
+		void addItem(Item& item);
 		template <class MobType>
-		void addMonster(MobType& monster)
-		{
-			monsters.push_back(std::shared_ptr<MobType>(monster));
-		}
+		void addMonster(MobType& monster);
 		//update
-		void update()
-		{
-			updateBullets();
-			updateMonsters();
-			updateItems();
-		}
-		void updateBullets()
-		{
-			for (auto iter = bullets.begin(); iter < bullets.end();)
-			{
-				auto bullet = *iter;
-				if (!((bullet->getPos().x >= -SCREENBORDER && bullet->getPos().x <= SCREENBORDER) && (bullet->getPos().y >= -SCREENBORDER && bullet->getPos().y <= SCREENBORDER)))
-				{
-					iter = bullets.erase(iter);
-				}
-				else
-				{
-					iter++;
-				}
-			}
-		}
-		void updateMonsters()
-		{
-			for (auto iterM = monsters.begin(); iterM < monsters.end(); iterM++)
-			{
-				auto monster = *iterM;
-				for (auto iterB = bullets.begin(); iterB < bullets.end(); iterB++)
-				{
-					auto bullet = *iterB;
-					if (CM::getInstance()->checkCircleCollision(monster.get(), bullet.get()))
-					{
-						SM::getInstance()->stopAndPlaySound("zombieHit");
-
-						float damage = bullet->getDamage();
-						iterB = bullets.erase(iterB) - 1;
-						monster->hit(damage);
-						if (monster->checkDie())
-						{
-							SM::getInstance()->stopAndPlaySound("zombieDie");
-							
-							iterM = monsters.erase(iterM) - 1;
-							ScoreManager::getInstance()->addKill(1);
-							break;
-						}
-					}
-				}
-			}
-		}
-		void updateItems()
-		{
-			for (auto iter = items.begin(); iter < items.end();iter++)
-			{
-				auto item = *iter;
-				if (CM::getInstance()->checkCircleCollision(player.get(), item.get()))
-				{
-					iter = items.erase(iter) - 1;
-					player->increasePower();
-				}
-			}
-		}
+		void update();
+		void updateBullets();
+		void updateMonsters();
+		void updateItems();
 	};
 }
